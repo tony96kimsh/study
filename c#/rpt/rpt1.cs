e71baa45da6aadf5d0022782479524a96466e1d7
tony96kimsh <tony96kimsh@gmail.com>
2025-05-03 21:52:00 +0900

    Delete obsolete C# project files and solution configurations for WindowsFormsApp7 and MyFirstApp. Add new rpt1.cs file implementing Shape and Circle classes with area calculation.

---
+++ c#/rpt/rpt1.cs
@@ -0,0 +1,22 @@
+/*
+도형을 나타내는 Shape 클래스와 그의 하위 클래스인 Circle 클래스가 있습니다.
+Shape 클래스에는 GetArea() 메서드가 있습니다. Circle 클래스에서 이 메서드를 오버라이딩하여 원의 넓이를 구하고 반환하도록 정의합니다. 
+(원의 넓이 공식은 반지름 * 반지름 * 3.14입니다.) Shape와 Circle 클래스를 각각 정의하고 GetArea 함수의 오버라이딩 관계를 정의한 후,
+ Main 함수 안에서 Circle 객체를 생성하고 원의 넓이를 구하여 화면에 출력하시오.
+
+도형을 나타내는 Shape 클래스와 그의 하위 클래스인 Circle 클래스가 있습니다. 
+Shape 클래스에는 GetArea() 메서드가 있습니다. 
+Circle 클래스에서 이 메서드를 오버라이딩하여 원의 넓이를 구하고 반환하도록 정의합니다.
+(원의 넓이 공식은 반지름 * 반지름 * 3.14입니다.)
+
+Shape와 Circle 클래스를 각각 정의하고 GetArea 함수의 오버라이딩 관계를 정의한 후, Main 함수 안에서 Circle 객체를 생성하고 원의 넓이를 구하여 화면에 출력하시오.
+*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        console.WriteLine('Hello world')
+    }
+}
