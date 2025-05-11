d367e2121b26236265fd5493ad1722bc87f209cb
tony96kimsh <tony96kimsh@gmail.com>
2025-05-11 18:57:29 +0900

    Remove unused delegate examples and related solution files; add abstract class and interface examples for smartphone and animal concepts; implement delegate-based compression program with callback functionality.

---
+++ c#/code/04-sealed/Q1.cs
@@ -0,0 +1,28 @@
+﻿/* 
+은행 시스템에서 `AccountSecurity`라는 클래스는 보안상 민감한 기능을 포함한다.
+이 클래스는 외부에서 절대로 상속되어서는 안 되며, 독립적으로만 사용되어야 한다.
+이러한 요구사항을 만족하는 `AccountSecurity` 클래스를 정의하라.
+ */
+using System;
+
+namespace MyApp
+{
+//[접근 제한자] [기타 수식어] 반환형 이름(...)
+// public sealed class MyClass { }       // 클래스 선언
+// public virtual void Speak() { }       // 가상 메서드
+// public override void Speak() { }      // 오버라이드
+// public sealed override void Speak() { } // 오버라이드 방지
+// protected abstract void Draw();       // 추상 메서드
+
+    public sealed class AccountSecurity 
+    {
+        public AccountSecurity () {Console.WriteLine("Create System to protect Bank");}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountSecurity accountSecurity = new AccountSecurity();
+        }
+    }
+}
---
+++ c#/code/04-sealed/Q3.cs
@@ -0,0 +1,34 @@
+﻿/* 
+게임에서 `FinalBoss` 클래스는 고정된 능력치를 가지며, 상속을 통해 수정되면 게임 밸런스에 문제가 생긴다.
+이러한 클래스를 봉인하여 더 이상 상속할 수 없도록 코드를 완성하라.
+*/
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public sealed class FinalBoss
+        {
+            public FinalBoss ()
+            {
+                Console.WriteLine("최종보스 등장");
+            }
+        }
+        // Error! cannot derive from sealed type
+        public class superVilein : FinalBoss
+        {
+            public superVilein ()
+            {
+                Console.WriteLine("최최종 뇌절 보스 등장");
+            }
+        }
+        
+        static void Main (string[] args) 
+        {
+            FinalBoss finalBoss = new FinalBoss();            
+
+        }
+    }
+}
