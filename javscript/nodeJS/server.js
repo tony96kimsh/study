80befc51ecf893dee32ad89d7603795b5e665c2f
Tony <tony96kimsh@gmail.com>
2025-05-30 16:52:10 +0900

    Add date input field to enter form, update server logic for saving and listing posts, and enhance views with Bootstrap styling
    - Added a date input field to the enter form in `enter.ejs`.
    - Updated the server to handle the new date field when saving posts.
    - Implemented rendering of posts in a table format in `list.ejs`.
    - Enhanced both `enter.ejs` and `list.ejs` with Bootstrap for better styling.
    - Added AJAX functionality for post deletion in `list.ejs`.
    - Updated package dependencies in `package.json` and `package-lock.json`.

--- javscript/nodeJS/server.js
+++ javscript/nodeJS/server.js
@@ -1,47 +1,75 @@
+const port = 7233;
+//express
+const express = require('express');
+const app = express();
+
+// 몽고DB 키 환경 변수 처리
 require('dotenv').config();
 
-
-// MongoDB + Node.js 접속 코드
 const mongoclient = require('mongodb').MongoClient;
 const url = process.env.MONGO_URL;
-// const url = 'mongodb+srv://tony96kimsh:tonyPw123@cluster0.b6e7jna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const ObjId = require('mongodb').ObjectId;
 let mydb;
 
+
+// body-parse
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: true}));
 
 mongoclient.connect(url)
   .then(client => {
     console.log('몽고DB 접속 성공');
     mydb = client.db('myboard');
-    
 
-    app.listen(9500, function() {
-    console.log("포트 9500으로 서버 대기중...");
+    app.listen(port, function() {
+    console.log("포트 port으로 서버 대기중...");
+    console.log("http://localhost:" + port);
   });
 })
 .catch(err => {
-  console.log(err);;
+  console.log(err);
 })
 
 
-const express = require('express');
-const app = express();
-
-// 루트 페이지
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/enter', function(req, res){
-  res.sendFile(__dirname + '/enter.html');
+  res.render('enter.ejs');
 })
 
-app.get('/list', function(req, res) {  
-  // conn.query("SELECT * FROM post", function(err, rows, field) {
-  // if(err) throw err;
-  // console.log(rows);
-  // })
+app.set('view engine', 'ejs');
 
-  mydb.collection('post').find().toArray().then(result => {
+
+app.post('/save', function(req, res) {
+  console.log(req.body);
+  console.log(req.body.content);
+  console.log(req.body.someDate);
+  
+  mydb.collection('post')
+    .insertOne({
+        title: req.body.title, content: req.body.content
+        ,date : req.body.someDate
+     })
+    .then(result => {
       console.log(result);
+      console.log('데이터 추가 성공');
     })
 });
+
+app.get('/list', function(req, res) {  
+  mydb.collection('post').find().toArray().then(result => {
+      console.log(result);
+      res.render('list.ejs', {data: result});
+    })
+})
+
+app.post("/delete", function(req, res) {
+  console.log(req.body._id);
+  req.body._id = new ObjId(req.body._id)
+  mydb.collection('post').deleteOne(req.body)
+  .then(result => {
+      console.log("삭제완료");
+    })
+})
